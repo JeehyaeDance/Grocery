2a5e999c727fb838712b912f5cc772b061db72a2
JeehyaeDance <jeehyae91@gmail.com>
2021-03-07 12:48:05 -0800

    move out files from server folder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 require("dotenv").config()
 import express from "express"
+import expressSession from 'express-session'
 
 const app = express()
 const port = process.env.SERVER_PORT
+
+app.use(express.static(__dirname));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const session = expressSession({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false})
 
 app.get('/', (req, res) => {
   res.send('Hello from Bananas!')
