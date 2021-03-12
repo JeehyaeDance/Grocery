22971a8bcf06d43c33027699bd62225ff838b495
DanceParty <keevandance@gmail.com>
2021-03-11 20:32:27 -0800

    fix formatting

--- src/queries.ts
+++ src/queries.ts
@@ -1,16 +1,16 @@
 import { Pool } from "pg"
 
 const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: Number(process.env.DB_PORT),
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  })
-  
-  db.on("error", (err) => {
-    console.log(err)
-  })
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
+db.on("error", (err) => {
+  console.log(err)
+})
