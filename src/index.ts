7411b32e0ba95741d8b11a8a5d657d50b98dd0ac
JeehyaeDance <jeehyae91@gmail.com>
2021-03-11 20:26:54 -0800

    install passposrt library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config()
 import express from "express"
 import expressSession from 'express-session'
+import passport from 'passport'
 
 const app = express()
 const port = process.env.SERVER_PORT
@@ -10,6 +11,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 const session = expressSession({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false})
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Hello from Bananas!')
